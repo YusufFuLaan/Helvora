ab17e2378d3e0a3c8fca89fc1822197ebf534688
YusufFuLaan <adebayoyusuf59@yahoo.com>
2025-02-22 10:32:53 +0100

    added: html css and js files

---
+++ faq.js
@@ -0,0 +1,8 @@
+// Hero Slide-----------------------------------------------------------------------------------------------------------
+
+document.querySelector("#menu").addEventListener("click", function () {
+  const menuLinks = document.querySelector(".header-links");
+  const icon = document.querySelector(".fa-bars");
+  menuLinks.classList.toggle("show");
+  icon.classList.toggle("fa-xmark");
+});
