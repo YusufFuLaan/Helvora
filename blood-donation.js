7dd356255fd606254993e28b0c46789b47a02219
YusufFuLaan <adebayoyusuf59@yahoo.com>
2025-03-03 13:05:40 +0100

    Added: Header and footer from main

---
+++ blood-donation.js
@@ -0,0 +1,8 @@
+// Header responsiveness-----------------------------------------------------------------------------------------------------------
+
+document.querySelector("#menu").addEventListener("click", function () {
+  const menuLinks = document.querySelector(".header-links");
+  const icon = document.querySelector(".fa-bars");
+  menuLinks.classList.toggle("show");
+  icon.classList.toggle("fa-xmark");
+});
